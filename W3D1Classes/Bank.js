142d2ae0a78c484782d0c1ba8a52824c3650bbe0
Habiba-Sultan-Rega <87673390+Habiba-Sultan-Rega@users.noreply.github.com>
2021-08-03 01:06:39 -0500

    bug fix

--- W3D1Classes/Bank.js
+++ W3D1Classes/Bank.js
@@ -1,45 +1,63 @@
-class Bank{
-    static accountNumber = 1;
+"use strict"
+class Bank {
+
     constructor() {
         this.accounts = [];
+        this.accountNumber = 1;
     }
-    addAccount() {
-            let newAccount = new Account(Bank.accountNumber++);
-            this.accounts.push(newAccount);
-         return this.accounts.length;
-        }
-    addSavingsAccount(interest) {
-            this.accounts.push(new SavingsAccount(Bank.accountNumber++, interest));
-              return this.accounts.length;
-          }
-    addCheckingAccount(overdraft) {
-           this.accounts.push(new CheckingAccount(Bank.accountNumber++, overdraft));
-           return this.accounts.length;
-             }
-    closeAccount(number) {
-            for (let i in this.accounts) {
-                if (this.accounts[i].getNumber() == number) {
-                    this.accounts.splice(i, 1);
-                    break;
-                }
-            }
-        }
-    static nextNumber() {
-            const nextNumber=accountNumber;
-            return ++nextNumber;
-        }
-    accountReport() {
-            let report = "";
-            for (let accounts of this.accounts) {
-                report += accounts.toString() + '\n';
-            }
-            return report;
-        }
-    endOfMonth() {
-            results = [];
-            for (let accounts of this.accounts) {
-                results.push(accounts.endOfMonth());
-            }
-        }
-}
 
+    addAccount() {
+        let acc = new Account(this.accountNumber);
+        this.accounts.push(acc);
+        this.accountNumber += 1;
+
+        return this.accounts.length;
+    }
+
+    addSavingsAccount(interest) {
+
+        let acc = new SavingsAccount(this.accountNumber, interest);
+        this.accounts.push(acc);
+        this.accountNumber += 1;
+
+        return this.accounts.length;
+
+    }
+
+    addCheckingAccount(overdraft) {
+        let acc = new CheckingAccount(this.accountNumber, overdraft);
+        this.accounts.push(acc);
+        this.accountNumber += 1;
+
+        return this.accounts.length;
+    }
+
+    closeAccount(number) {
+        for (let i in this.accounts) {
+            if (this.accounts[i].getNumber() == number) {
+                this.accounts.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    accountReport() {
+        let st = "";
+        for (let acc of this.accounts) {
+            st += acc.toString() + '\n';
+        }
+
+        return st;
+    }
+
+    static nextNumber() {
+        return this.accountNumber;
+    }
+
+    endOfMonth() {
+        results = [];
+        for (let acc of this.accounts) {
+            results.push(acc.endOfMonth());
+        }
+    }
+}
--- W3D1Classes/test.js
+++ W3D1Classes/test.js
@@ -1,43 +1,43 @@
-describe("getNumber", function() {
+describe("Get Account Number", function() {
     it("gets account number of account", function() {
-        let acc = new Account(123);
-        assert.equal(acc.getNumber(), 123);
+        let accounts = new Account(1);
+        assert.equal(accounts.getNumber(), 1);
     })
 });
 
 describe("toString", function() {
     it("string representation of an account", function() {
-        let acc = new Account(123);
-        assert.equal(acc.toString(), "Account " + acc.getNumber() + ": balance " + acc.getBalance());
+        let accounts = new Account(1);
+        assert.equal(accounts.toString(), "Account " + accounts.getNumber() + ": balance " + accounts.getBalance());
     })
 });
 
 describe("deposit", function() {
     it("deposits money and update balance from account", function() {
-        let acc = new Account(123);
-        acc.deposit(100);
-        acc.deposit(100);
-        assert.equal(acc.getBalance(), 200);
-        assert.throws(() => acc.deposit(-1), Error);
+        let accounts = new Account(123);
+        accounts.deposit(100);
+        accounts.deposit(100);
+        assert.equal(accounts.getBalance(), 200);
+        assert.throws(() => accounts.deposit(-1), Error);
     })
 });
 
-describe("withdraw", function() {
+describe("Wthdraw", function() {
     it("withdraw money and update balance from account", function() {
-        let acc = new Account(123);
-        acc.deposit(100);
-        acc.deposit(100);
-        acc.withdraw(50);
-        assert.equal(acc.getBalance(), 150);
-        assert.throws(function() { acc.withdraw(200) }, Error);
+        let accounts = new Account(123);
+        accounts.deposit(100);
+        accounts.deposit(100);
+        accounts.withdraw(50);
+        assert.equal(accounts.getBalance(), 150);
+        assert.throws(function() { accounts.withdraw(200) }, Error);
     })
 });
 
 describe("getInterest", function() {
     it("returns the interest of a saving account", function() {
-        let sa = new SavingsAccount(123);
-        sa.setInterest(10);
-        assert.equal(sa.getInterest(), 10);
+        let saving_account = new SavingsAccount(123);
+        saving_account.setInterest(10);
+        assert.equal(saving_account.getInterest(), 10);
     });
 });
 
@@ -48,30 +48,30 @@ describe("toString", function() {
     })
 });
 
-describe("addInterest", function() {
+describe("adding Interest", function() {
     it("returns the interest of a saving account", function() {
-        let sa = new SavingsAccount(123, 10);
-        sa.deposit(100);
-        sa.addInterest();
-        assert.equal(sa.getBalance(), 110);
+        let saving_account = new SavingsAccount(123, 10);
+        saving_account.deposit(100);
+        saving_account.addInterest();
+        assert.equal(saving_account.getBalance(), 110);
     });
 });
 
-describe("withdraw", function() {
+describe("Withdrawing", function() {
     it("withdraw money from a checking account", function() {
-        let ca = new CheckingAccount(123, 100);
-        ca.deposit(100);
-        ca.withdraw(50);
-        assert.equal(ca.getBalance(), 50);
+        let checking_account = new CheckingAccount(1, 100);
+        checking_account.deposit(100);
+        checking_account.withdraw(50);
+        assert.equal(checking_account.getBalance(), 50);
     });
 });
 
-describe("withdraw", function() {
+describe("Withdrawing accounts", function() {
     it("withdraw money from a checking account below balance", function() {
-        let ca = new CheckingAccount(123, 100);
-        ca.deposit(100);
-        ca.withdraw(150);
-        assert.equal(ca.getBalance(), -50);
+        let checking_account = new CheckingAccount(1, 100);
+        checking_account.deposit(100);
+        checking_account.withdraw(150);
+        assert.equal(checking_account.getBalance(), -50);
         assert.throws(function() { withdraw(100) }, Error);
 
     });
@@ -79,12 +79,12 @@ describe("withdraw", function() {
 
 describe("toString", function() {
     it("string representation of a Checking account", function() {
-        let acc = new CheckingAccount(123, 100);
-        assert.equal(acc.toString(), "Account " + acc.getNumber() + ": balance " + acc.getBalance() + `overdraft: ${acc.getOverdraft()}`);
+        let account = new CheckingAccount(123, 100);
+        assert.equal(account.toString(), "Account " + account.getNumber() + ": balance " + account.getBalance() + `overdraft: ${account.getOverdraft()}`);
     })
 });
 
-describe("addAccount", function() {
+describe("adding Account", function() {
     it("adds account to bank and return count of accounts", function() {
         let bank = new Bank();
         assert.equal(bank.addAccount(), 1);
@@ -93,7 +93,7 @@ describe("addAccount", function() {
     })
 });
 
-describe("addSavingAccount", function() {
+describe("adding Saving Accounts", function() {
     it("adds saving account to bank and return count of accounts", function() {
         let bank = new Bank();
         assert.equal(bank.addSavingsAccount(10), 1);
@@ -102,7 +102,7 @@ describe("addSavingAccount", function() {
     })
 });
 
-describe("addCheckingAccount", function() {
+describe("Add Checking Accounts", function() {
     it("adds checking account to bank and return count of accounts", function() {
         let bank = new Bank();
         assert.equal(bank.addCheckingAccount(10), 1);
@@ -111,7 +111,7 @@ describe("addCheckingAccount", function() {
     })
 });
 
-describe("closeAccount", function() {
+describe("Closing Accounts", function() {
     it("removed an account from list", function() {
         let bank = new Bank();
         bank.addCheckingAccount(10);
@@ -127,7 +127,7 @@ describe("closeAccount", function() {
     })
 });
 
-describe("accountReport", function() {
+describe("Account Report", function() {
     it("returns a String with each account on its own line", function() {
         let bank = new Bank();
         bank.addAccount();
@@ -137,14 +137,14 @@ describe("accountReport", function() {
 });
 
 
-describe("endOfMonth", function() {
+describe("End of Month", function() {
     it("return empty endOfMonth report for account", function() {
         let acc = new Account(10);
         assert.equal(acc.endOfMonth(), '');
     })
 });
 
-describe("endOfMonth", function() {
+describe("End of Month", function() {
     it("return formatted endOfMonth report for saving account", function() {
         let acc = new SavingsAccount(1, 10);
         acc.deposit(100);
@@ -152,7 +152,7 @@ describe("endOfMonth", function() {
     })
 });
 
-describe("endOfMonth", function() {
+describe("End of Month", function() {
     it("return formatted endOfMonth report for checking account", function() {
         let acc = new CheckingAccount(1, 100);
         acc.deposit(50);
