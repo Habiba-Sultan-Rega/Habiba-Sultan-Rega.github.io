05a564945e3b0c18e96956215877bfb9384887f2
Habiba-Sultan-Rega <87673390+Habiba-Sultan-Rega@users.noreply.github.com>
2021-08-04 21:36:23 -0500

    maze game java event handler used(animate and moseenter, moseout)

---
+++ W3D3Maze/maze.js
@@ -0,0 +1,32 @@
+$(function () {
+    "use strict";
+           $('#start').click(function () {
+                    $("#start").animate({ left:155+'px'});
+               })
+            $('#start').click(function () {
+                 $("#start").animate({ top:54+'px'});
+                 });
+            $('#start').click(function () {
+                    $("#start").animate({ left:303+'px'});
+               });
+            $('#start').click(function () {
+                $("#start").animate({ top:205+'px'});
+              });
+           $('#start').click(function () {
+                $("#start").animate({ left:458+'px'});
+                 setTimeout(function() {
+                               $("#end").animate({ left:540+'px'});
+                               $(".boundary").css("background-color", "green");
+                               $("#status").text("You win !!!  :D :D")
+                               .css({"color":"red","font-size":"30px"});
+                               }, 1900);
+            });
+
+            $(".boundary").mouseenter(function () {
+                        $(".boundary").css("background-color", "yellow");
+                    });
+
+             $(".boundary").mouseleave(function () {
+                        $(".boundary").css("background-color", "red");
+                    });
+});
